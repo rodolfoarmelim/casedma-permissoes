5768b01c29d209586a176876396b9c3635bb410b
Rodolfo Del Santo <rdelsant@emeal.nttdata.com>
2024-03-26 14:52:59 -0300

    Subindo a tabela sor

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
-#resource "aws_lakeformation_permissions" "database_permission_sor" {
-#    principal   = var.producer_role_arn_mesh
-#    permissions = ["DESCRIBE", "CREATE_TABLE", "ALTER", "DROP"]
-#
-#    database {
-#      name       = var.database_sor
-#      catalog_id = var.control_account
-#    }
-#}
-#
-#resource "aws_lakeformation_permissions" "database_permision_sot" {
-#    principal   = var.producer_role_arn_mesh
-#    permissions = ["DESCRIBE", "CREATE_TABLE", "ALTER", "DROP"]
-#
-#    database {
-#        name       = var.database_sot
-#        catalog_id = var.control_account
-#    }
-#}
-#
-#resource "aws_lakeformation_permissions" "database_permission_spec" {
-#    principal   = var.producer_role_arn_mesh
-#    permissions = ["DESCRIBE", "CREATE_TABLE", "ALTER", "DROP"]
-#
-#    database {
-#        name       = var.database_spec
-#        catalog_id = var.control_account
-#    }
-#}
+resource "aws_lakeformation_permissions" "database_permission_sor" {
+    principal   = var.producer_role_arn_mesh
+    permissions = ["DESCRIBE", "CREATE_TABLE", "ALTER", "DROP"]
+
+    database {
+      name       = var.database_sor
+      catalog_id = var.control_account
+    }
+}
+
+resource "aws_lakeformation_permissions" "database_permision_sot" {
+    principal   = var.producer_role_arn_mesh
+    permissions = ["DESCRIBE", "CREATE_TABLE", "ALTER", "DROP"]
+
+    database {
+        name       = var.database_sot
+        catalog_id = var.control_account
+    }
+}
+
+resource "aws_lakeformation_permissions" "database_permission_spec" {
+    principal   = var.producer_role_arn_mesh
+    permissions = ["DESCRIBE", "CREATE_TABLE", "ALTER", "DROP"]
+
+    database {
+        name       = var.database_spec
+        catalog_id = var.control_account
+    }
+}
 
 resource "aws_glue_catalog_table" "tabela_sor" {
   depends_on = ["aws_lakeformation_permissions.database_permission_sor"]
@@ -176,5 +176,6 @@ resource "aws_glue_catalog_table" "tabela_sor" {
     }
   }
 }
+
 
 
